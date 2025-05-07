f57d8ecdd97a1492f4b1013530ba89d75d3b75db
Ahmed Emad Mohammed Abdelaziz Samra <ahmedemad23@mailbox.unideb.hu>
2025-05-07 19:39:29 +0200

    Fixing mapping error

--- kashmit5/src/components/module-select/ModuleSelect.js
+++ kashmit5/src/components/module-select/ModuleSelect.js
@@ -1,21 +1,28 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Skeleton, styled, Tooltip } from "@mui/material";
 import { Box, Stack } from "@mui/system";
 import CustomImageContainer from "../CustomImageContainer";
 import { setSelectedModule } from "../../redux/slices/utils";
-import { useSelector } from "react-redux";
-import { useRouter } from "next/router";
 
 const Container = styled(Stack)(({ theme }) => ({
   zIndex: "999",
   alignItems: "center",
   justifyContent: "center",
+  //top: 90,
+  //right: 950,
+  
+  //boxShadow: "0px 0px 29.7006px rgba(71, 71, 71, 0.1)",
+  //background: theme.palette.background.paper,
+  //borderTopLeftRadius: "29px",
+  //borderBottomLeftRadius: "29px",
   flexDirection: "row",
   gap: "30px",
   [theme.breakpoints.down("sm")]: {
-    display: "none",
+  display: "none",
   },
 }));
+
+
 
 const ModuleContainer = styled(Box)(({ theme, selected }) => ({
   zIndex: 100000,
@@ -26,15 +33,21 @@ const ModuleContainer = styled(Box)(({ theme, selected }) => ({
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
+  //backgroundColor: "rgba(227, 227, 227, 0.2)",
+  //border: "2px solid",
   borderColor: selected
     ? theme.palette.primary.main
     : theme.palette.background.paper,
   background:
     selected &&
     "radial-gradient(50% 50% at 50% 50%, rgba(0, 202, 108, 0) 0%, rgba(0, 255, 137, 0.2) 100%)",
-  flexDirection: "row",
-  gap: "10px",
+  flexDirection: "row", // Ensures that icon and text are in a row
+  gap: "10px",  // Adds some space between the image and the text
+  justifyContent: "flex-start"
 }));
+
+
+
 
 export const zoneWiseModule = (data) => {
   let currentZoneIds = undefined;
@@ -55,93 +68,68 @@ const ModuleSelect = ({
   configData,
   dispatch,
 }) => {
-  const router = useRouter();
-
-  const filteredModules = zoneWiseModule(data);
-
-  // Auto-select first module if none selected
-  useEffect(() => {
-    const { module_id, moduleType } = router.query;
-
-    if (!module_id || !moduleType) {
-      const fallbackModule = filteredModules?.[0];
-      if (fallbackModule) {
-        router.replace(
-          {
-            pathname: router.pathname,
-            query: {
-              ...router.query,
-              module_id: fallbackModule.id,
-              moduleType: fallbackModule.module_type,
-            },
-          },
-          undefined,
-          { shallow: true }
-        );
-
-        dispatch(setSelectedModule(fallbackModule));
-        moduleSelectHandler(fallbackModule);
-      }
-    }
-  }, [router.query, filteredModules]);
-
   const handleModuleSelect = (item) => {
     dispatch(setSelectedModule(item));
     moduleSelectHandler(item);
-    router.push(
-      {
-        pathname: router.pathname,
-        query: {
-          ...router.query,
-          module_id: item.id,
-          moduleType: item.module_type,
-        },
-      },
-      undefined,
-      { shallow: true }
-    );
   };
-
   return (
     <Container p=".8rem" spacing={0}>
-      {filteredModules.length > 0 ? (
-        filteredModules.map((item, index) => (
-          <Tooltip title={item?.module_name} key={index} placement="bottom">
-            <ModuleContainer
-              selected={
-                item?.module_type === selectedModule?.module_type &&
-                item?.id === selectedModule?.id
-              }
-              onClick={() => handleModuleSelect(item)}
+      {data ? (
+        zoneWiseModule?.(data)?.map((item, index) => {
+          return (
+            <Tooltip
+              title={item?.module_name}
+              key={index}
+              placement="bottom"
             >
-              <CustomImageContainer
-                src={`${configData?.base_urls?.module_image_url}/${item?.icon}`}
-                width="36px"
-                height="36px"
-                alt={item?.module_name}
-                objectFit="contained"
-              />
-              <ModuleTitle>{item?.module_name}</ModuleTitle>
-            </ModuleContainer>
-          </Tooltip>
-        ))
+              <ModuleContainer
+                selected={
+                  item?.module_type === selectedModule?.module_type &&
+                  item?.id === selectedModule?.id
+                }
+                onClick={() => handleModuleSelect(item)}
+              >
+                <CustomImageContainer
+                  src={`${configData?.base_urls?.module_image_url}/${item?.icon}`}
+                  width="36px"
+                  height="36px"
+                  alt="mobile"
+                  objectFit="contained"
+                />
+
+                <ModuleTitle>{item?.module_name}</ModuleTitle> 
+       
+                
+              </ModuleContainer>
+              
+            </Tooltip>
+          );
+        })
       ) : (
-        [...Array(5)].map((_, index) => (
-          <Skeleton key={index} width="40px" height="40px" variant="rectangle" />
-        ))
+        <>
+          {[...Array(5)].map((item, index) => (
+            <Skeleton
+              key={index}
+              width="40px"
+              height="40px"
+              variant="rectangle"
+            />
+          ))}
+        </>
       )}
     </Container>
   );
 };
-
+// Styled component for the module title (name)
 const ModuleTitle = styled("span")(({ theme }) => ({
-  fontSize: "14px",
-  color: theme.palette.text.primary,
+  marginleft: "auto",  // Space between icon and title
+  fontSize: "14px",  // Font size for the title
+  textAlign: "center",  // Center the title text
+  color: theme.palette.text.primary,  // Use the theme's text color
   fontWeight: "bold",
-  whiteSpace: "nowrap",
-  overflow: "visible",
-  textOverflow: "clip",
-  flexShrink: 0,
-}));
+  whiteSpace: "nowrap",  // Prevent title from wrapping to the next line
+  textOverflow: "ellipsis",  // Show ellipsis for overflowing text
+  lineHeight: "1.2",  // Control line height for better text readability
 
+}));
 export default ModuleSelect;
